0df22d702a5434dd3801581424a86c6598d18316
Liora13 <liorabr@yahoo.com>
2022-07-18 15:49:50 +0200

    usercontroller+form

---
+++ src/Controller/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\User;
+use App\Form\RegisterFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/inscriptpion", name="user_register", methods={"GET|POST"})
+     */
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        #1 - Instanciation de class
+        $user = new User();
+
+        # 2 - creation du formulaire
+        $form = $this->createForm(RegisterFormType::class, $user)
+        ->handleRequest($request);
+
+        #4- si le form est soumis ET valide
+        if($form->isSubmitted() && $form->isValid()){
+            $user->setRoles(['ROLE_USER']);
+            $user->setCreatedAt(new DateTime());
+            $user->setUpdatedAt(new DateTime());
+
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', "Vous vous êtes inscrit avec succès !");
+            return $this->redirectToRoute('app_login');
+        }
+
+        #3 - On retourne la vue du formulaire
+        return $this->render("user/register.html.twig", [
+            'form' => $form->createView()
+        ]);
+    }
+   
+}
