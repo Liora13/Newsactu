0795f17c9962515ae12cdbbb2da03fd56a0fa4aa
Liora13 <liorabr@yahoo.com>
2022-07-22 14:43:18 +0200

    create Profile + change password function

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use DateTime;
 use App\Entity\User;
+use App\Entity\Article;
+use App\Form\ChangePasswordFormType;
 use App\Form\RegisterFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
@@ -46,6 +49,50 @@ class UserController extends AbstractController
         return $this->render("user/register.html.twig", [
             'form' => $form->createView()
         ]);
+    }
+    /**
+     * @Route("/profile/mon-espace-perso", name="show_profile", methods={"GET"})
+     */
+    public function showProfile(EntityManagerInterface $entityManager): Response
+    {
+        $articles = $entityManager->getRepository(Article::class)->findBy(['author' => $this->getUser()]);
+
+        return $this->render("user/show_profile.html.twig", [
+            'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/profile/changer-mon-mot-depasse", name="change_password", methods={"GET|POST"})
+     */
+    public function changePassword(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, Request $request): Response
+    {
+        $form = $this->createForm(ChangePasswordFormType::class)
+            ->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            /** @var User $user */
+            $user = $entityManager->getRepository(User::class)->findOneBy(['id' => $this->getUser()]);
+
+
+            $user->setUpdatedAt(new DateTime());
+
+            $user->setPassword($passwordHasher->hashPassword(
+                $user, $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', "Votre mot de passe a bien été changé");
+            return $this->redirectToRoute('show_profile');
+    
+        }
+        return $this->render('user/chage_password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+            
     }
    
 }
